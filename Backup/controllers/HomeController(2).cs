b7f5df0a33df1ff3fe3822df78a56f8493fbc047
thfthf2 <thfthf2@163.com>
2018-11-02 22:24:09 +0800

    Initialize 1

---
+++ Backup/controllers/HomeController(2).cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Web.Mvc;
+
+using NStore.Core;
+using NStore.Services;
+using NStore.Web.Framework;
+
+namespace NStore.Web.Mobile.Controllers
+{
+    /// <summary>
+    /// 首页控制器类
+    /// </summary>
+    public partial class HomeController : BaseMobileController
+    {
+        /// <summary>
+        /// 首页
+        /// </summary>
+        public ActionResult Index()
+        {
+            //首页的数据需要在其视图文件中直接调用，所以此处不再需要视图模型
+            return View();
+        }
+    }
+}
